0c56746e80777b3bbe9158166687cf07f8cf3912
harrisdurrani <harris.durrani@hotmail.com>
2024-04-19 16:43:22 -0700

    cleaned books csv and dropped image columns

--- books_data/parser.py
+++ books_data/parser.py
@@ -5,17 +5,20 @@ import pandas as pd
 
 
 
-with open('books_data/books.csv', newline='') as file:
-    # columns = ["ISBN","Book-Title","Book-Author","Year-Of-Publication","Publisher","Image-URL-S","Image-URL-M","Image-URL-L", 'text1', 'text2', 'text3']
-    reader = csv.reader(file, delimiter=';')
-    first_row = next(reader)
+# with open('books_data/books.csv', newline='') as file:
+#     # columns = ["ISBN","Book-Title","Book-Author","Year-Of-Publication","Publisher","Image-URL-S","Image-URL-M","Image-URL-L", 'text1', 'text2', 'text3']
+#     reader = csv.reader(file, delimiter=';')
+#     first_row = next(reader)
 
-    df = pd.DataFrame(reader, columns=["ISBN","Book-Title","Book-Author","Year-Of-Publication","Publisher","Image-URL-S","Image-URL-M","Image-URL-L", 'text1', 'text2', 'text3'])
-cleanded_df = df.drop(columns=['text1', 'text2', 'text3'], axis=1)
+#     df = pd.DataFrame(reader, columns=["ISBN","Book-Title","Book-Author","Year-Of-Publication","Publisher","Image-URL-S","Image-URL-M","Image-URL-L", 'text1', 'text2', 'text3'])
+# cleanded_df = df.drop(columns=['text1', 'text2', 'text3'], axis=1)
 
 
-print(cleanded_df)
+# print(cleanded_df)
 
 # Currently the data is incorrectly formatted and causes errors due to the semicolon delimiter. A script will be made to clean out the data so the read csv function can be used
-# df = pd.read_csv('books_data/books.csv', sep=';', encoding='latin-1')
-# print(df.to_string())
+
+
+df = pd.read_csv('books_data/books.csv', sep=';', encoding='latin-1', low_memory=False)
+cleaned_df = df.drop(columns=['Image-URL-S', 'Image-URL-M','Image-URL-L'], axis=1)
+print(cleaned_df.head())
